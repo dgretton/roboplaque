6659ebb761fa790d38bae25c039f33bffd673a23
Dana Gretton <dana.gretton@gmail.com>
2020-04-18 09:53:50 -0400

    turboroboplaque: access 2 24-well plates simultaneously

--- roboplaque.py
+++ roboplaque.py
@@ -4,6 +4,7 @@ import os
 import sys
 import time
 import logging
+from math import ceil
 
 this_file_dir = os.path.dirname(os.path.abspath(__file__))
 method_local_dir = os.path.join(this_file_dir, 'method_local')
@@ -32,27 +33,29 @@ if __name__ == '__main__':
     simulation_on = debug or '--simulate' in sys.argv
     num_dilutions = 4
     num_plates = 1 if '--plates' not in sys.argv else int(sys.argv[sys.argv.index('--plates') + 1])
+    assert num_plates <= 4
     num_skips = 0 if '--skip' not in sys.argv else int(sys.argv[sys.argv.index('--skip') + 1])
     culture_vol = 20 # uL
-    hard_agar_vol = 220 # uL
+    hard_agar_vol = 250 # uL
     soft_agar_vol = 200 # uL
     culture_stock_vol = 300 # uL
-    dipenses_per_prep_tip = int(1000/1.1)//int(hard_agar_vol)
+    dipenses_per_prep_tip = 3 #int(1000/1.1)//int(hard_agar_vol)
     culture_asps_per_stock = int(culture_stock_vol)//int(culture_vol*1.25) # room for over-aspiration error
     #print('dipenses_per_prep_tip', dipenses_per_prep_tip)
     #print('culture_asps_per_stock', culture_asps_per_stock)
+    #print('Number of filled', culture_stock_vol, 'uL tubes of bacterial culture needed for this run:', int(ceil(num_plates*24/culture_asps_per_stock)))
 
     lmgr = LayoutManager(LAYFILE)
 
     dilution_array = lmgr.assign_unused_resource(ResourceType(Plate96, 'dilutions'))
-    agar_site = lmgr.assign_unused_resource(ResourceType(Plate96, 'agar'))
+    agar_sites = resource_list_with_prefix(lmgr, 'agar_', Plate96, 4)
     culture_site = lmgr.assign_unused_resource(ResourceType(Plate96, 'culture'))
-    plates = resource_list_with_prefix(lmgr, 'assay_plate_', Plate96, num_plates) # TODO: Plate24, num_plates)
-    agar_tips = resource_list_with_prefix(lmgr, 'disposable_tips_', Tip96, 2)
-    culture_tips = resource_list_with_prefix(lmgr, 'standard_tips_', Tip96, 2)
+    plates = resource_list_with_prefix(lmgr, 'assay_plate_', Plate24, num_plates)
+    agar_tips = resource_list_with_prefix(lmgr, 'disposable_tips_', Tip96, 1)
+    culture_tips = resource_list_with_prefix(lmgr, 'standard_tips_', Tip96, 1)
+    #eject_site = None if '--test' in sys.argv else lmgr.assign_unused_resource(ResourceType(Tip96, 'test_eject_site'))
 
-    agar_poss = ([(agar_site, 0), (agar_site, 1), (agar_site, 3), (agar_site, 4)]*((num_dilutions+1)//4))[:num_dilutions]
-    skip_4th_chan = lambda n: n
+    all_agar_poss = [(agar_site, n) for agar_site in agar_sites for n in (0, 1)]
 
     def agar_tips_gen():
         while True:
@@ -68,29 +71,20 @@ if __name__ == '__main__':
                     yield disp_tip_rack, i
     culture_tips_gen = culture_tips_gen()
 
-    def agar_tip_batches():
+    def gen_culture_poss():
         while True:
-            yield skip_4th_chan([next(agar_tips_gen) for _ in range(num_dilutions)])
-    agar_tip_batches = agar_tip_batches()
-
-    def culture_tip_batches():
-        while True:
-            yield skip_4th_chan([next(culture_tips_gen) for _ in range(num_dilutions)])
-    culture_tip_batches = culture_tip_batches()
-
-    def culture_poss():
-        while True:
-            for start_i in range(0, 96, num_dilutions):
-                for _ in range(culture_asps_per_stock):
+            for _ in range(culture_asps_per_stock):
+                for start_i in range(0, 16, num_dilutions):
                     yield [(culture_site, start_i + j) for j in range(num_dilutions)]
-    culture_poss = culture_poss()
+    gen_culture_poss = gen_culture_poss()
 
     def pos_batches(): # tuples (positions to aspirate from, positions to dispense into)
-        for plate, quadrant_start in zip(plates, (0, 8*6, 4, 8*6+4)): # truncate to len(plates)
+        for plate, quadrant_start in zip(plates, (0, 4, 8*6, 8*6+4)): # truncate to len(plates)
             for col in range(6): # column of 24-well plate
                 yield ([(dilution_array, quadrant_start + col*8 + i) for i in range(num_dilutions)],
-                       [(plate, idx_24_to_96(col*4 + i)) for i in range(num_dilutions)])
-    pos_batches = pos_batches()
+                       [(plate, col*4 + i) for i in range(num_dilutions)],
+                       lambda: next(gen_culture_poss))
+    pos_batches = list(pos_batches())
     
     def replace_agar_tips():
         n = 0
@@ -98,16 +92,12 @@ if __name__ == '__main__':
             yield n % dipenses_per_prep_tip == 0
             n += 1
     replace_agar_tips = replace_agar_tips()
-    
-    dummy_24_plate = Plate24('')
-    def idx_24_to_96(idx):
-        col96, row96 = (c*2 for c in dummy_24_plate.well_coords(idx)) # upsample a 6x4 well plate to be accessed by a 12x8 template
-        return col96*8 + row96
 
     with HamiltonInterface(simulate=simulation_on) as ham_int:
         ham_int.set_log_dir(os.path.join(local_log_dir, 'hamilton.log'))
         initialize(ham_int)
-        #hepa_on(ham_int, simulate=int(simulation_on))
+        hepa_on(ham_int, 30, simulate=int(simulation_on))
+        exit()
         try:
             errmsg_str = ''
             start_time = time.time()
@@ -116,19 +106,21 @@ if __name__ == '__main__':
             if fileflag('stop'):
                 clear_fileflag('stop'); exit()
 
-            def new_tips(ttype='agar'):
+            def new_tips(num=num_dilutions, ttype='agar'):
                 while True:
                     try:
                         if ttype == 'agar':
-                            tip_pick_up(ham_int, next(agar_tip_batches))
+                            tip_batch = [next(agar_tips_gen) for _ in range(num)]
                         elif ttype == 'culture':
-                            tip_pick_up(ham_int, next(culture_tip_batches))
+                            tip_batch = [next(culture_tips_gen) for _ in range(num)]
                         else:
                             raise ValueError()
+                        tip_pick_up(ham_int, tip_batch)
                         break
                     except pyhamilton.NoTipError:
                         initialize(ham_int) # eject tips if present
                         continue
+                return tip_batch
 
             #########################
             # Begin non-boilerplate #
@@ -136,33 +128,59 @@ if __name__ == '__main__':
 
             agar_class = 'HighVolumeFilter_Agar'
             std_class = 'StandardVolumeFilter_Water_DispenseJet_Empty_no_transport_vol'
-            skips = num_skips
-            for dilutions, plate_wells in pos_batches:
-                if skips > 0:
-                    logging.info('Skip ' + str(dilutions) + ', ' + str(plate_wells))
-                    skips -= 1
-                    continue
+
+            def tend_to(dilutions, plate_wells, culture_poss): # deal with up to 8 at the same time
+                print(culture_poss)
+                num_dilutions = len(dilutions)
+                print(num_dilutions)
+                agar_poss = all_agar_poss[:num_dilutions]
                 if prep_hard_agar:
                     logging.info('\n##### Filling assay plates with hard agar.')
                     if next(replace_agar_tips):
                         tip_eject(ham_int)
-                        new_tips()
-                        aspirate(ham_int, agar_poss, [hard_agar_vol*dipenses_per_prep_tip*1.1]*num_dilutions, liquidClass=agar_class) # Aspirate 10% more to avoid bubbles
-                    dispense(ham_int, plate_wells, [hard_agar_vol]*num_dilutions, liquidClass=agar_class)
-                    continue
+                        new_tips(num_dilutions)
+                        aspirate(ham_int, agar_poss, [hard_agar_vol*dipenses_per_prep_tip*1.3]*num_dilutions, liquidClass=agar_class) # Aspirate 10% more to avoid bubbles
+                    dispense(ham_int, plate_wells, [hard_agar_vol]*num_dilutions, liquidHeight=6, liquidClass=agar_class)
+                    return
                 logging.info('\n##### Moving culture into dilution wells.')
-                new_tips('culture')
-                aspirate(ham_int, next(culture_poss), [culture_vol]*num_dilutions, liquidClass=std_class)
+                new_tips(num_dilutions, 'culture')
+                aspirate(ham_int, culture_poss, [culture_vol]*num_dilutions, liquidClass=std_class)
                 dispense(ham_int, dilutions, [culture_vol]*num_dilutions, liquidClass=std_class)
                 tip_eject(ham_int)
                 logging.info('\n##### Moving agar into dilution tubes.')
-                new_tips()
+                new_tips(num_dilutions)
                 aspirate(ham_int, agar_poss, [soft_agar_vol + 50]*num_dilutions, liquidClass=agar_class)
-                dispense(ham_int, skip_4th_chan(dilutions), skip_4th_chan([soft_agar_vol + 50]*num_dilutions), liquidHeight=6, liquidClass=agar_class)
+                dispense(ham_int, dilutions, [soft_agar_vol + 50]*num_dilutions, liquidHeight=6, liquidClass=agar_class)
                 logging.info('\n##### Moving finished dilutions into plate wells.')
-                aspirate(ham_int, skip_4th_chan(dilutions), skip_4th_chan([soft_agar_vol]*num_dilutions), liquidClass=agar_class)
-                dispense(ham_int, skip_4th_chan(plate_wells), skip_4th_chan([soft_agar_vol]*num_dilutions), liquidHeight=6, liquidClass=agar_class)
+                aspirate(ham_int, dilutions, [soft_agar_vol]*num_dilutions, liquidClass=agar_class)
+                dispense(ham_int, plate_wells, [soft_agar_vol]*num_dilutions, liquidHeight=6, liquidClass=agar_class)
                 tip_eject(ham_int)
+
+            todo_plates = plates[:]
+            while todo_plates:
+                this_round_plates = []
+                for _ in range(2):
+                    try:
+                        this_round_plates.append(todo_plates.pop(0))
+                    except IndexError:
+                        break
+                this_round_batches = []
+                for pos_batch in pos_batches:
+                    _, ((extract_plate, _), *_), _ = pos_batch
+                    if extract_plate in this_round_plates:
+                        this_round_batches.append(pos_batch)
+                if len(this_round_batches) > 6: # divide it in half
+                    if len(this_round_batches)%2 != 0:
+                        raise RuntimeError()
+                    half_idx = len(this_round_batches)//2
+                    for (dilutions1, plate_wells1, culture_poss1), (dilutions2, plate_wells2, culture_poss2) in zip(this_round_batches[:half_idx], this_round_batches[half_idx:]):
+                        dilutions = dilutions1 + dilutions2
+                        plate_wells = plate_wells1 + plate_wells2
+                        culture_poss = culture_poss1() + culture_poss2()
+                        tend_to(dilutions, plate_wells, culture_poss)
+                else:
+                    for dilutions, plate_wells, culture_poss in this_round_batches:
+                        tend_to(dilutions, plate_wells, culture_poss())
             if prep_hard_agar:
                 tip_eject(ham_int)
 
